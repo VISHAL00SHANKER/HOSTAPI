69d6b44b50ddb99ea57fcc4027f74723e0c08aa6
vishaluu <93986854+VISHAL00SHANKER@users.noreply.github.com>
2023-12-05 03:14:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+
+const PORT =  process.env.PORT || 5000;
+
+const products_routes = require("./routes/products");
+ 
+
+app.get("/", (req, res) => {
+    res.send("abc");
+
+});
+
+
+// middleware  or set router
+app.use("/api/products", products_routes);
+
+const start = async () => {
+
+    try {
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(PORT, () => {
+            console.log(`${PORT} yes i am contected`);
+        });
+
+    }catch(error){
+        console.log(error)
+    }
+};
+
+start();
